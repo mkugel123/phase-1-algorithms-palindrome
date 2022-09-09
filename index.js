c95e4ed311cf00961c1561ca3b14911e70b1e63e
Zalman <moishkugel@gmail.com>
2022-09-09 15:16:04 -0400

    commit tree 1d1f5cb10fbb373438fc3f289a04df2ad10168fe

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const arrayOfLetters = word.split('');
+  const halfOfArrayIndex = Math.floor(arrayOfLetters.length/2);
+  const indexOfLastLetter = arrayOfLetters.length - 1;
+  let boolean
+  if (arrayOfLetters.length <= 1){
+    boolean = true
+  } else
+  for (let i = 0; i < halfOfArrayIndex; i++){
+    if (arrayOfLetters[i] == arrayOfLetters[indexOfLastLetter - i]){
+      boolean = true
+    } else if (!(arrayOfLetters[i] === arrayOfLetters[indexOfLastLetter - i])){
+      boolean = false
+      return boolean
+    }
+  }
+  return boolean
 }
 
 /* 
   Add your pseudocode here
+  -turn word into array of letters
+  -find out index of letter that is either half or one
+  less than half of word
+  -find out if first letter is same as last and so on
 */
 
 /*
@@ -14,9 +33,13 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("madamimadam"));
 
-  console.log("");
+  // console.log("Expecting: array of letters");
+  // console.log("=>", isPalindrome("hello"))
+
+  // console.log("Expecting: 4");
+  // console.log("=>", isPalindrome("hello"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
